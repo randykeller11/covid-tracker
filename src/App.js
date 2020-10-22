cecb3bfa1fffb2bfda0597f3ba5dd076a9e68a04
Randy <randykeller1193@gmail.com>
2020-10-22 18:13:03 -0400

    fixed drop down bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { MenuItem, FormControl, Select } from "@material-ui/core";
+import { MenuItem, FormControl, Select, Menu } from "@material-ui/core";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -13,7 +14,7 @@ function App() {
           const _countries = data.map((country) => (
             {
             name: country.country,
-            valuse: country.countryInfo.iso2,
+            value: country.countryInfo.iso2,
           }
           
           ));
@@ -21,14 +22,21 @@ function App() {
         });
     };
     getCountriesData();
-  }, [countries]);
+  }, []);
+
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+
+    setCountry(countryCode);
+  };
 
   return (
     <div className="app">
       <div className="app__header">
         <h1>Covid-19 TRACKER</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value="abc">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value='worldwide'>Worldwide</MenuItem>
             {/* loop through all the countries and show
              drop down list of options*/}
             {countries.map((country) => (
