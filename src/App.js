4ccfd5eac3804bfcb1b1a39bf11cfb9a7a7412f4
Randy <randykeller1193@gmail.com>
2020-10-22 16:17:39 -0400

    npm installed material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <h1>connected</h1>
+      {/* Header */}
+      {/* Title and Select input dropdown field*/}
+
+
+      {/* InfoBoxs*/}
+      {/* InfoBoxs*/}
+      {/* InfoBoxs*/}
+      
+      {/*Table */}
+      {/* Graph*/}
+
+      
+      {/* Map*/}
     </div>
   );
 }
